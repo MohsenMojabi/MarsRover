2a4c801a366a18b18f8ff48ba1e1ec764fe3ac7d
popcom <mojabi.mohsen@gmail.com>
2022-06-22 10:24:15 +0200

    Adding comment for api

--- MarsRover.WebApi/Controllers/MovementsController.cs
+++ MarsRover.WebApi/Controllers/MovementsController.cs
@@ -21,6 +21,16 @@ namespace MarsRover.WebApi.Controllers
             _mediat = mediat;
         }
 
+        /// <summary>
+        /// Translate recieved commands for Mars Roter to move
+        /// </summary>
+        /// <param name="edges">Mars Rover wraps at edges. Edges are 2 positive integer numbers like 5 5</param>
+        /// <param name="obstacles">Mars Rover stops at obstacles. Obstacles are list of pair positive integer numbers like (2 3)(6 1)(9 7)</param>
+        /// <param name="initialPosition">Initial position of Mars Rover. Initial position contains 2 positive integer numbers like 4 8</param>
+        /// <param name="commands">Commands could contain F, f, B, b, R, r, L, l characters for moving forward, backward, turning right, turning left like FrlLLRFffBRbbLllrrR</param>
+        /// <returns>
+        /// The last position that Mars Rover could move to if no exception had been throwed.
+        /// </returns>
         [HttpPost]
         public async Task<ActionResult<CommandsResult>> Move(string edges, string obstacles, string initialPosition, string commands)
         {
